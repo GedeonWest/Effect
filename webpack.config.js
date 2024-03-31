6893f5f03a350cb21a2fcd39ed0f636291f04930
Vladislav N <vladislav.n@ruport.ru>
2024-03-31 22:52:27 +0300

    finish cases (no logic)

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-import Dotenv from "dotenv-webpack"
-import path from "path"
-import MiniCssExtractPlugin from "mini-css-extract-plugin"
-import HtmlWebpackPlugin from "html-webpack-plugin"
-import CopyPlugin from "copy-webpack-plugin"
+import Dotenv from 'dotenv-webpack';
+import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 
 export default {
   // Define the entry points of our application (can be multiple for different sections of a website)
   entry: {
-    main: "./src/js/main.js",
+    main: './src/js/main.js',
   },
 
   // Define the destination directory and filenames of compiled resources
   output: {
-    filename: "js/[name].js",
-    path: path.resolve(process.cwd(), "./public"),
+    filename: 'js/[name].js',
+    path: path.resolve(process.cwd(), './public'),
   },
 
   // Define development options
-  devtool: "source-map",
+  devtool: 'source-map',
 
   // Define loaders
   module: {
@@ -27,13 +27,11 @@ export default {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [
-              "@babel/preset-env"
-            ]
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       // CSS, PostCSS, and Sass
       {
@@ -41,31 +39,29 @@ export default {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 2,
               sourceMap: true,
               url: false,
-            }
+            },
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: [
-                  "autoprefixer",
-                ]
-              }
-            }
+                plugins: ['autoprefixer'],
+              },
+            },
           },
-          "sass-loader"
+          'sass-loader',
         ],
       },
       // File loader for images
       {
         test: /\.(jpg|jpeg|png|git|svg)$/i,
-        type: "asset/resource",
-      }
+        type: 'asset/resource',
+      },
     ],
   },
 
@@ -73,39 +69,41 @@ export default {
   plugins: [
     // Load .env file for environment variables in JS
     new Dotenv({
-      path: "./.env"
+      path: './.env',
     }),
 
     // Extracts CSS into separate files
     new MiniCssExtractPlugin({
-      filename: "css/[name].css",
-      chunkFilename: "[id].css"
+      filename: 'css/[name].css',
+      chunkFilename: '[id].css',
     }),
 
     // Copy images to the public folder
     new CopyPlugin({
       patterns: [
         {
-          from: "src/images",
-          to: "images",
-        }
-      ]
+          from: 'src/images',
+          to: 'images',
+        },
+      ],
     }),
 
     // Inject styles and scripts into the HTML
     new HtmlWebpackPlugin({
-      template: path.resolve(process.cwd(), "index.html")
-    })
+      template: path.resolve(process.cwd(), 'index.html'),
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'cases.html',
+      template: path.resolve(process.cwd(), 'cases.html'),
+    }),
   ],
 
   // Configure the "webpack-dev-server" plugin
   devServer: {
     static: {
-      directory: path.resolve(process.cwd(), "public")
+      directory: path.resolve(process.cwd(), 'public'),
     },
-    watchFiles: [
-      path.resolve(process.cwd(), "index.html")
-    ],
+    watchFiles: [path.resolve(process.cwd(), 'index.html')],
     compress: true,
     port: process.env.PORT || 9090,
     hot: true,
@@ -113,6 +111,6 @@ export default {
 
   // Performance configuration
   performance: {
-    hints: false
-  }
+    hints: false,
+  },
 };
