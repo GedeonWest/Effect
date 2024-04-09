36276ec725cb83b4f4c71cc6d6d581f16c5e2763
Vladislav N <vladislav.n@ruport.ru>
2024-04-09 23:43:56 +0300

    add new block and cards logic

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import '../scss/main.scss';
 import Masonry from 'masonry-layout';
 import ticker from './ticker.js';
 import header from './header.js';
+import cardsRange from './cardsRange.js';
+import { initSliders } from './feedbackSwiper.js';
 
 // Load Bootstrap init
 import { initBootstrap } from './bootstrap.js';
@@ -16,3 +18,5 @@ initBootstrap({
 
 header();
 ticker();
+cardsRange();
+initSliders();
