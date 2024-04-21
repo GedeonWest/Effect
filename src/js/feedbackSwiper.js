5bff0d80337f21b349306ba32dcbb1a655c53e73
Vladislav N <vladislav.n@ruport.ru>
2024-04-21 17:56:29 +0300

    add fixes

--- src/js/feedbackSwiper.js
+++ src/js/feedbackSwiper.js
@@ -6,27 +6,26 @@ let contactsSwiper;
 
 export const initSliders = function () {
   contactsSwiper = new SwiperCore('.feedback-swiper', {
-    slidesPerView: 4,
-    slidesPerGroup: 1,
+    slidesPerView: 3,
     spaceBetween: 24,
     centeredSlides: false,
     loop: true,
     slideToClickedSlide: true,
     breakpoints: {
       320: {
-        slidesPerView: 3,
+        slidesPerView: 1,
         spaceBetween: 16,
       },
       768: {
-        slidesPerView: 3,
+        slidesPerView: 2,
         spaceBetween: 24,
       },
       1025: {
-        slidesPerView: 4,
+        slidesPerView: 3,
         spaceBetween: 24,
       },
       1600: {
-        slidesPerView: 5,
+        slidesPerView: 3,
         spaceBetween: 24,
         loop: true,
       },
